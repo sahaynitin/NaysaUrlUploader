1e4c65e37b0147d4de1c7e7186cb9fabe8fb0763
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-10 09:01:49 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -326,7 +326,7 @@ async def echo(bot, update):
         time.sleep(1)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(""),
+            text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
